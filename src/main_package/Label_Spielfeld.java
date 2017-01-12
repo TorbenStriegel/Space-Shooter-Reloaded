5ba947900c5ad92f0176cd013fcf80d6e8d542e9
Torben <torben.striegel@gmail.com>
2017-01-12 19:40:40 +0100

    Kommentare

--- src/main_package/Label_Spielfeld.java
+++ src/main_package/Label_Spielfeld.java
@@ -53,13 +53,13 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		if (!var.verloren) {
+		if (!var.verloren) { //wenn verloren Screenblack
 			g.drawImage(Var.background_1, var.background_x1, 0, Var.spielfeld_screenwidth, Var.spielfeld_screenheight,
 					null);
 			g.drawImage(Var.background_2, var.background_x2, 0, Var.spielfeld_screenwidth, Var.spielfeld_screenheight,
 					null);
 
-			if (gegner) {
+			if (gegner) { // Wenn gegner da sind zeichnen
 				for (int i = 0; i < var.getGegnerArray().length; i++) {
 					g.drawImage(var.getGegnerArray()[i].getImage(), var.getGegnerArray()[i].gibxpos(), var.getGegnerArray()[i].gibypos(),
 							null);
@@ -67,14 +67,14 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 			}
 			g.drawImage(raumschiff.gibRaumschiff_bild(), raumschiff.gibRaumschiff_xpos(),
 					raumschiff.gibRaumschiff_ypos(), null);
-			if (raumschiff.gibRaumschiff_schildanzeigen()) {
+			if (raumschiff.gibRaumschiff_schildanzeigen()) {  // Schild nur anzeigen wenn es nicht zerstört ist
 				g.drawImage(raumschiff.gibRaumschiffschild_bild(),
 						raumschiff.gibRaumschiff_xpos() + raumschiff.gibRaumschiff_bild().getWidth() - 10,
 						raumschiff.gibRaumschiff_ypos() - 15, 50, raumschiff.gibRaumschiff_bild().getHeight() + 30,
 						null);
 			}
 
-			for (int i = 0; i < var.getGeschossliste().size(); i++) {
+			for (int i = 0; i < var.getGeschossliste().size(); i++) {       // Zeichne Schüsse
 				try {
 
 					g.drawImage(var.getGeschossliste().get(i).gibImage(),
@@ -84,7 +84,7 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 				}
 			}
 
-			for (int i = 0; i < var.getItemliste().size(); i++) {
+			for (int i = 0; i < var.getItemliste().size(); i++) {   // zeichne Items
 				try {
 
 					g.drawImage(var.getItemliste().get(i).gibBild(), var.getItemliste().get(i).gibXpos(),
@@ -94,28 +94,28 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 			}
 
 			g.setColor(Color.GRAY);
-			g.fillRect(20, 35, raumschiff.getRaumschiff_Leben_Default(), 20);
+			g.fillRect(20, 35, raumschiff.getRaumschiff_Leben_Default(), 20);   // zeige maximale Leben vom Raumschiff
 
 			g.setColor(new Color(190, 60, 60));
-			g.fillRect(20, 35, raumschiff.gibRaumschiff_leben(), 20);
+			g.fillRect(20, 35, raumschiff.gibRaumschiff_leben(), 20);    // zeige aktuelle Leben vom Raumschiff
 
 			g.setColor(Color.GRAY);
-			g.fillRect(20, 60, raumschiff.gibRaumschiff_schildMax(), 20);
+			g.fillRect(20, 60, raumschiff.gibRaumschiff_schildMax(), 20);   // zeige maximale Leben vom Schild
 
 			g.setColor(new Color(60, 60, 190));
-			g.fillRect(20, 60, raumschiff.gibRaumschiff_schild(), 20);
+			g.fillRect(20, 60, raumschiff.gibRaumschiff_schild(), 20);		// zeige aktuelle Leben vom Schild
 
 			g.setColor(Color.YELLOW);
 			g.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
-			g.drawString("Score:" + score.getFinalScore(), Var.spielfeld_screenwidth / 2 - 50, 60);
+			g.drawString("Score:" + score.getFinalScore(), Var.spielfeld_screenwidth / 2 - 50, 60);  // Score
 			g.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
-			g.drawString("Treffergenauigkeit:" + score.getTreffergenauigkeit() + "%",
+			g.drawString("Treffergenauigkeit:" + score.getTreffergenauigkeit() + "%",   // Treffergenauigkeit
 					Var.spielfeld_screenwidth / 2 - 120, 100);
 			g.setColor(Color.GREEN);
-			if(!var.survival){
+			if(!var.survival){   // wenn Survival Modus werden nicht alle Elemente angezeigt
 			g.drawString("Zeit:" + spieltimer.getZeit_Millisekunden(), Var.spielfeld_screenwidth / 2, 140);
 			
-			if (spieltimer.getZeit_Millisekunden() <= 10) {
+			if (spieltimer.getZeit_Millisekunden() <= 10) {   // wenn nur noch 10 sek, zeige zeit Groß im Spielfeld
 				g.setFont(new Font("Arial", Font.BOLD, 90));
 				g.drawString("" + spieltimer.getZeit_Millisekunden(), Var.spielfeld_screenwidth / 2,
 						Var.spielfeld_screenheight / 2);
@@ -123,9 +123,9 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 			}
 			g.setColor(Color.RED);
 			g.setFont(new Font("Arial", Font.BOLD, 30));
-			g.drawString("Level:" + gegner_Level.getLevel(), (Var.spielfeld_screenwidth - 150), 35);
+			g.drawString("Level:" + gegner_Level.getLevel(), (Var.spielfeld_screenwidth - 150), 35);  // Zeige Level oben Rechts
 
-			if (Var.zeigeLebenGegner) {
+			if (Var.zeigeLebenGegner) {    // wenn Endboss, zeige Leben vom Gegner
 				g.setColor(Color.RED);
 				g.setFont(new Font("Arial", Font.BOLD, 50));
 				g.drawString("Boss:" + var.getGegnerArray()[0].gibleben() + " Leben", (Var.spielfeld_screenwidth / 2 - 160),
@@ -134,10 +134,10 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 
 			beendet = false;
 		}
-		if (var.pause && !var.startCounter) {
+		if (var.pause && !var.startCounter) {      // Zeige Pause und mach Bildschirm grau
 			g.setColor(new Color(100, 100, 100, 128));
 			g.fillRect(0, 0, Var.spielfeld_screenwidth, Var.spielfeld_screenheight);
-		} else if (var.startCounter) {
+		} else if (var.startCounter) {   // Counter beim start und nach Pause 3,2,1
 			g.setColor(Color.RED);
 			g.setFont(new Font("Arial", Font.BOLD, 140));
 			g.drawString("" + start_Counter.getCounter(), (Var.spielfeld_screenwidth / 2 - 30),
@@ -145,11 +145,11 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 			g.setColor(new Color(100, 100, 100, 128));
 			g.fillRect(0, 0, Var.spielfeld_screenwidth, Var.spielfeld_screenheight);
 
-		} else if (var.verloren && !Var.survival) {
+		} else if (var.verloren && !Var.survival) {    // Schluss Screen (erreichtes Level, weiter button)
 
 			g.setColor(Color.BLACK);
 			g.fillRect(0, 0, Var.spielfeld_screenwidth, Var.spielfeld_screenheight);
-			if (var.timer_finish) {
+			if (var.timer_finish) {    // wenn gewonnen
 				g.setColor(Color.GREEN);
 				g.setFont(new Font("Arial", Font.BOLD, 60));
 				g.drawString("Dein Platz:" + score.getPlatzierung(), (Var.spielfeld_screenwidth / 2 - 200),
@@ -159,7 +159,7 @@ public class Label_Spielfeld extends JLabel { // Erbt von JLabel, damit alle
 					zeichneWeiterButton();
 				}
 
-			} else {
+			} else {     // wenn verloren
 				g.setColor(Color.RED);
 				g.setFont(new Font("Arial", Font.BOLD, 60));
 				g.drawString("GAME OVER", (Var.spielfeld_screenwidth / 2 - 210), (Var.spielfeld_screenheight / 2 - 30));
